015b87c419d803beff1d8189a526ff1827eff120
unknown <NeixLawliet>
2024-05-30 11:00:34 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".konten-slider").bxSlider({
+        Controls: true,
+        auto: true,
+        pager: true,
+        mode: 'fade',
+        captions: true,
+        speed: 500,
+    }
+  );
+});
